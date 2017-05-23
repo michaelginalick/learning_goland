c44c851059d5f8064a69fe647d6bca5120569ea6
michael ginalick <michael.ginalick@contextmediainc.com>
2017-05-22 19:33:29 -0500

    wip

--- go_book/popcount/popcount.go
+++ go_book/popcount/popcount.go
@@ -13,14 +13,10 @@ func init() {
 
 func PopCount(x uint64) int {
 
- return int(pc[byte(x>>(0*8))] +
-  pc[byte(x>>(1*8))] +
-  pc[byte(x>>(2*8))] +
-  pc[byte(x>>(3*8))] +
-  pc[byte(x>>(4*8))] +
-  pc[byte(x>>(5*8))] +
-  pc[byte(x>>(6*8))] +
-  pc[byte(x>>(7*8))]
- )
+ // var i uint = 0
 
+ for i uint = 0; i < 9; i++ {
+   x += int(pc[byte(x>>(i*8))])
+ }
+ return int(pc[byte(x)])
 }
