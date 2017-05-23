f547e99ee82318f6fb0a08a3fb1ca68d13cae451
michael ginalick <michael.ginalick@contextmediainc.com>
2017-05-22 20:23:38 -0500

    for loop implemented

--- go_book/popcount.go
+++ go_book/popcount.go
@@ -4,8 +4,20 @@ var pc [256]byte
 
 func init() {
 	for i := range pc {
-	
+
 		pc[i] = pc[i/2] + byte(i&1)
 	}
 
+}
+
+
+func PopCount(x uint64) int {
+
+ var i uint = 0
+ y := pc[byte(x)]
+ for i < 9 {
+  y += pc[byte(x>>(i*8))]
+ }
+
+ return int(y)
 }
