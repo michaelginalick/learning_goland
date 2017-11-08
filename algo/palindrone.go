69440b77335cae887cf10c1627eb9cd87b94a9fa
Allison Zuzelo <allisonzuzelo@Allisons-MBP.attlocal.net>
2017-11-07 19:38:00 -0600

    palindrone

---
+++ algo/palindrone.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	r_string := "Hello World"
+	
+	c := pallindrone(r_string)
+	fmt.Println(c)	
+}
+
+func pallindrone(r_string string) bool {
+
+	rune := []rune(r_string)
+
+	for i, j := 0, len(rune)-1; i < len(rune)/2; i, j = i+1, j-1 {
+		if rune[i] != rune[j] {
+		   return false
+		}
+	}
+
+	return true
+
+}
