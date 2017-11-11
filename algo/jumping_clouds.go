2bd6453655c5839deb6f17ba516368d49a908223
Michael Ginalick <michaelginalick@Michaels-MBP.attlocal.net>
2017-11-11 16:02:51 -0600

    jumping clounds

---
+++ algo/jumping_clouds.go
@@ -0,0 +1,32 @@
+package main
+import "fmt"
+
+func main() {
+    var d int
+    fmt.Scan(&d)
+    
+    arr := make([]int, d)
+    
+
+    for i := 0; i < len(arr); i++ {
+        var a int
+        fmt.Scan(&a)
+        arr[i] = a
+    }
+    
+    var numOfJumps, i int
+    
+    for {
+        if i + 2 < len(arr) && arr[i+2] == 0 {
+            i +=2
+        } else if i + 1 < len(arr) {
+            i++
+        } else {
+            break
+        }
+
+        numOfJumps++
+    }
+    
+    fmt.Print(numOfJumps)
+}
