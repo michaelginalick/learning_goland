6b64b974ba64c92f8b822dd4d2d20215a661a5f9
Michael Ginalick <michaelginalick@Michaels-MBP.attlocal.net>
2017-11-27 21:39:27 -0600

    recurrsive comma seperator

---
+++ algo/comma.go
@@ -0,0 +1,27 @@
+package main
+
+import(
+  "fmt"
+)
+
+func main() {
+
+  s := "12345678910"
+
+  ret := comma(s)
+  fmt.Print(ret) //12,345,678,910
+
+}
+
+
+
+func comma(s string) string {
+  n := len(s)
+  if n < 3 {
+          return s
+  }
+
+  return comma(s[:n-3]) + "," + s[n-3:]
+  // s[:n-3] = 12345678
+  // s[n-3:] = 910
+}
