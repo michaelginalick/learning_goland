014d1cd4afa027f21ca2959a72eaf25dc62a8ff7
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2017-12-09 16:45:33 -0600

    merge sort

--- algo/camelCase.go
+++ algo/camelCase.go
@@ -7,15 +7,12 @@ import(
 func main() {
  //Enter your code here. Read input from STDIN. Print output to STDOUT
     var strInput string
-    
     fmt.Scan(&strInput)
-        
     caps := 1
-    
     for i := 0; i<len(strInput); i++{
         if string(strInput[i]) == strings.ToUpper(string(strInput[i])) {
             caps++
         }
-    } 
+    }
     fmt.Print(caps)
 }
