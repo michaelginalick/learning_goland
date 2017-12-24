c9960d3d393af48d052e9de4b897226a908013cc
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2017-12-24 18:18:12 -0500

    delting nodes from linked list

--- algo/linked_list.go
+++ algo/linked_list.go
@@ -17,11 +17,81 @@ func main() {
 	mike := &student{33, 200, "mike", nil}
   studentList := addNode(mike)
 	greg := &student{34, 443, "greg", nil}
+	james := &student{45, 554, "james", nil}
+	marshall := &student{56, 123, "marshall", nil}
+	allison := &student{54, 234, "allison", nil}
   studentList = addNodeBeginning(greg, studentList)
+	studentList = addNodeBeginning(james, studentList)
+	studentList = addNodeBeginning(marshall, studentList)
+  studentList = addNodeEnd(allison, studentList)
+
+  printList(studentList)
+
+	fmt.Println("\nFind student-----------------")
+	findStudent := findNodeByName("mike", studentList)
+	fmt.Println(findStudent.name)
+	fmt.Println("\nDelete student")
+	studentList = deleteNodeByName("allison", studentList)
+	printList(studentList)
+}
+
+
+func printList(studentList *student) *student{
+	for s := studentList; s != nil; s = s.next {
+	  fmt.Println(s.name, s.age, s.weight)
+	}
+	return studentList
+}
+
+func deleteNodeByName(name string, studentList *student) *student{
+  if studentList == nil {
+	  return studentList
+	}
+  s := studentList
+  //delete head
+  if s.name == name {
+  	s = s.next
+  	return s
+  }
+  // delete rest
+	for s.next != nil {
+	  if s.next.name == name {
+		  s.next = s.next.next
+			return studentList
+		}
+		s = s.next
+	}
+	return studentList
+}
+
+
+func findNodeByName(name string, studentList *student) *student {
+	if studentList == nil {
+    return studentList
+	}
+
+	for s := studentList; s !=nil; s = s.next {
+		if s.name == name {
+			return s
+			break
+		}
+	}
+	return studentList
+}
+
+func addNodeEnd(newStudent, studentList *student) *student {
+  if studentList == nil {
+	  return studentList
+	}
 
 	for s := studentList; s != nil; s = s.next {
-	  fmt.Println(s.age, s.weight, s.name)
+	  if s.next == nil {
+		  s.next = newStudent
+			return studentList
+		}
 	}
+
+	return studentList
 }
 
 
