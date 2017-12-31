37b22a94db9614ce704aefacbd96ef7506a1d369
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2017-12-30 22:37:58 -0600

    wip

--- algo/comma.go
+++ algo/comma.go
@@ -8,18 +8,29 @@ func main() {
 
 	s := "12345678910"
 
-	ret := comma(s)
-	fmt.Print(ret) //12,345,678,910
+  ret := commaRecursive(s)
+  fmt.Print(ret) //12,345,678,910
 
 }
 
-func comma(s string) string {
+
+func commaRecursive(s string) string {
+
 	n := len(s)
 	if n < 3 {
 		return s
 	}
-	fmt.Print(s[:n-3])
-	return comma(s[:n-3]) + "," + s[n-3:]
-	// s[:n-3] = 12345678
-	// s[n-3:] = 910
+
+	return commaRecursive(s[:n-3]) + "," + s[n-3:]
 }
+
+func commaIterative(x string) string {
+	for i := len(x)-1; i > 0; i-- {
+		
+		if i % len(x) == 0 {
+			x = x[:i] + "," + x[i:]
+		}
+	}
+	return x
+}
+
