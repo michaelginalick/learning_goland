8a7c24b0005342addd78308b6cd7936b19c7995b
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2018-01-21 22:10:55 -0600

    data set up almost complete

--- algo/bfs/bfs.go
+++ algo/bfs/bfs.go
@@ -5,7 +5,6 @@ import (
     "fmt"
     "os"
     "io/ioutil"
-    // "strings"
     "encoding/json"
 )
 
@@ -19,6 +18,9 @@ type File struct {
 
 type Node struct {
   value string
+  edges []*Node
+  searched bool
+  parent string
 }
 
 type Graph struct {
@@ -33,35 +35,55 @@ func (g *Graph) addNode(n *Node) {
   if g.graph == nil {
     g.graph = make(map[string]*Node)
   }
+
   g.graph[title] = n
+}
+
+
+func (g *Graph) getNode(s string) *Node {
+  for i := 0; i < len(g.nodes); i++ {
+    if g.nodes[i].value == s {
+      return g.nodes[i]
+    }
+  }
+  var actorNode *Node = new(Node)
+  return actorNode
+}
+
+func (n *Node) addEdge(actorNode *Node) {
+  n.edges = append(n.edges, actorNode)
+  actorNode.edges = append(actorNode.edges, n)
 }
 
 
 func main() {
   var file File
+  var g *Graph = new(Graph)
 
   jsonFile, err := os.Open("bacon.json")
-  
-  var g *Graph = new(Graph)
   
   if err != nil {
     fmt.Println(err)
   }
+
   defer jsonFile.Close()
   byteValue, _ := ioutil.ReadAll(jsonFile)
   json.Unmarshal(byteValue, &file)
 
 
   for i := 0; i < len(file.Movies); i++ {
-    var n *Node = new(Node)
-    n.value = file.Movies[i].Title
-    g.addNode(n)
+    var movieNode *Node = new(Node)
+    movieNode.value = file.Movies[i].Title
+    g.addNode(movieNode)
     
     for j := 0; j < len(file.Movies[i].Cast); j++ {
-      var n *Node = new(Node)
-      n.value = file.Movies[i].Cast[j]
-      g.addNode(n)
+      actorNode := g.getNode(file.Movies[i].Cast[j])
+
+      actorNode.value = file.Movies[i].Cast[j]
+      g.addNode(actorNode)
+      movieNode.addEdge(actorNode)
+      
     }
   }
-  fmt.Println(g.graph)
+  fmt.Println(g)
 }
