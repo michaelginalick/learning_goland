18e7f6cd76037bbf54295a3a1da17356ae7bf2e5
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2018-02-21 21:19:40 -0600

    format

--- server/main.go
+++ server/main.go
@@ -1,13 +1,14 @@
 package main
 
 import (
-    "net/http"
-    "github.com/gorilla/mux"
-    "log"
-    "fmt"
-    _ "github.com/lib/pq"
-    "database/sql"
-    "encoding/json"
+  "database/sql"
+  "encoding/json"
+  "fmt"
+  "github.com/gorilla/mux"
+  _ "github.com/lib/pq"
+  "log"
+  "net/http"
+  "time"
 )
 
 const (
@@ -18,20 +19,20 @@ const (
   dbname   = "pathlogic"
 )
 
-
 type Site struct {
-  Id int `json:"id"`
+  Id   int    `json:"id"`
   Name string `json:"string"`
 }
 
 type JSONData struct {
-  SiteID int `json:"siteId"`
+  SiteID       int       `json:"siteId"`
+  ModifiedDate time.Time `json:"modifiedDate"`
 }
 
 func openDB() *sql.DB {
   var db *sql.DB
 
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+ "password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
   db, err := sql.Open("postgres", psqlInfo)
   if err != nil {
     panic(err)
@@ -50,28 +51,28 @@ func SiteShow(w http.ResponseWriter, r *http.Request) {
   vars := mux.Vars(r)
   siteId := vars["siteId"]
   db := openDB()
-  
+
   err := db.QueryRow("select id from sites where id = $1", siteId).Scan(&s.Id)
 
-   if err != nil {
-      log.Fatalf("Failed to redirect stderr to file: %v", err)
-    }
+  if err != nil {
+    log.Fatalf("Failed to redirect stderr to file: %v", err)
+  }
 
-    if err != nil {
-      log.Printf("Failed to encode a JSON response: %v", err)
-      w.WriteHeader(http.StatusInternalServerError)
-      return
-    }
-    w.Header().Set("Content-Type", "application/json; charset=UTF-8")
-    jd.SiteID = s.Id
-    data, err := json.Marshal(jd)
+  if err != nil {
+    log.Printf("Failed to encode a JSON response: %v", err)
+    w.WriteHeader(http.StatusInternalServerError)
+    return
+  }
+  w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+  jd.SiteID = s.Id
+  jd.ModifiedDate = time.Now()
+  data, err := json.Marshal(jd)
 
-    if err != nil {
-        fmt.Println(err)
-    }
-    fmt.Fprintf(w, string(data))
+  if err != nil {
+    fmt.Println(err)
+  }
+  fmt.Fprintf(w, string(data))
 }
-
 
 func main() {
   router := mux.NewRouter().StrictSlash(true)
