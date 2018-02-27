8c7f2706e896fd52a926e45ad6fd309982fa4399
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2018-02-26 21:22:20 -0600

    wip

--- server/db/db.go
+++ server/db/db.go
@@ -1,37 +1,17 @@
 package db
 
-import (
-    "database/sql"
-    _ "github.com/lib/pq"
-    "log"
-)
+func openDB() *sql.DB {
+  var db *sql.DB
 
-const (
-  host     = "localhost"
-  port     = 5432
-  user     = "postgres"
-  password = "password"
-  dbname   = "pathlogic"
-)
-
-
-var db *sql.DB
-
-
-func initDB() {
-  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
-    "password=%s dbname=%s sslmode=disable",
-    host, port, user, password, dbname)
+  psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
   db, err := sql.Open("postgres", psqlInfo)
   if err != nil {
     panic(err)
   }
-  defer db.Close()
-
-  err = db.Ping()
-  if err != nil {
-    panic(err)
+  
+  if err = db.Ping(); err != nil {
+    log.Fatal(err)
   }
-
-  fmt.Println("Successfully connected!")
+  
+  return db
 }
