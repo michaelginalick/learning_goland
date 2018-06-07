4e639cf0e9ec2ebf34d9d75ca78a06c570c4101f
Michael Ginalick <michaelginalick@Michaels-MacBook-Pro.local>
2018-06-07 07:35:39 -0500

    centered the clock

--- pomodoro/transform/transform.go
+++ pomodoro/transform/transform.go
@@ -1,26 +1,22 @@
 package transform
 
-
-import(
+import (
 	tm "github.com/buger/goterm"
 )
 
-// ExtractNumbers : returns the ten and one place ints in a two digit number 
-func ExtractNumbers(twoDigitNumber int) (int, int){
-	
-	tenDigit := (twoDigitNumber/10)%10
-	onesDigit := (twoDigitNumber%10)
-	
+// ExtractNumbers : returns the ten and one place ints in a two digit number
+func ExtractNumbers(twoDigitNumber int) (int, int) {
+
+	tenDigit := (twoDigitNumber / 10) % 10
+	onesDigit := (twoDigitNumber % 10)
+
 	return tenDigit, onesDigit
 }
 
-// FormatNumber : prints the clock digits 
-func FormatNumber(minutesTens, minuteOnes, place, secondTens, secondOnes [5]string ) int {
-	var y int
-
-	for i:=0; i < len(minutesTens); i++ {
-		y, _ = tm.Println(minutesTens[i], minuteOnes[i], place[i], secondTens[i], secondOnes[i])
+// FormatNumber : prints the clock digits
+func FormatNumber(minutesTens, minuteOnes, place, secondTens, secondOnes [5]string) {
+	for i := 0; i < len(minutesTens); i++ {
+		tm.MoveCursorForward(60)
+		tm.Println(minutesTens[i], minuteOnes[i], place[i], secondTens[i], secondOnes[i])
 	}
-	return y
 }
-
